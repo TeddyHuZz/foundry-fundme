199adf8768c12ae84e1386fed073b843da0f8a66
TeddyHuZz <feigaming04XP@gmail.com>
2025-04-29 21:07:55 +0800

    Creating Test Functions

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -5,8 +5,19 @@ pragma solidity ^0.8.18;
 import {Test} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
 
-contract FundMeTest {
-    function setup() public {}
-    
-    function testMinimumDollarIsFice() public {}
+contract FundMeTest is Test {
+    FundMe fundMe; //Declare new instancecalled fundMe
+
+    function setUp() public {
+        fundMe = new FundMe(); //Deploy new FundMe contract
+    }
+
+    function testMinimumDollarIsFive() public view {
+        assertEq(fundMe.MINIMUM_USD(), 5); // Check if the minimum dollar amount is 5
+    }
+
+    function testOwnerIsMsgSender() public view {
+        assertEq(fundMe.i_owner(), msg.sender); // Check if the owner is the message sender
+    }
+
 }
