7317ed466783d439b5c6462b0ad6000bdebd61f2
TeddyHuZz <feigaming04XP@gmail.com>
2025-04-29 21:20:02 +0800

    Adding console log

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -2,14 +2,15 @@
 
 pragma solidity ^0.8.18;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
 
 contract FundMeTest is Test {
-    FundMe fundMe; //Declare new instancecalled fundMe
+    FundMe fundMe; //Declare new instance called fundMe
 
     function setUp() public {
         fundMe = new FundMe(); //Deploy new FundMe contract
+        console.log("GM");
     }
 
     function testMinimumDollarIsFive() public view {
@@ -17,7 +18,13 @@ contract FundMeTest is Test {
     }
 
     function testOwnerIsMsgSender() public view {
-        assertEq(fundMe.i_owner(), msg.sender); // Check if the owner is the message sender
+        console.log(msg.sender);
+        console.log(address(this));
+        //assertEq(fundMe.i_owner(), msg.sender); // Check if the owner is the message sender, msg.sender is the address that deploys the contract
+        assertEq(fundMe.i_owner(), address(this)); // Check if the owner is the address of this contract
     }
 
+    function testPriceFeedVersionIsAccurate() public view {
+        //uint256 version = fundMe.getVersion();
+    }
 }
